3bdf1786b2f4fbf72bd29c9a4c54662520e138d9
Vishal Sharma <visharma@tavisca.com>
2019-08-19 00:49:49 +0530

    Updated Project : Replaced Logger with System.out

--- src/main/java/com/tavisca/workshops/HttpServer/LogsUtilities/LogsWriter.java
+++ src/main/java/com/tavisca/workshops/HttpServer/LogsUtilities/LogsWriter.java
@@ -1,36 +1,35 @@
 package com.tavisca.workshops.HttpServer.LogsUtilities;
 
-import java.io.IOException;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 public class LogsWriter {
-    Logger logger;
+//    Logger logger;
 
     public LogsWriter(String className) {
-        logger = Logger.getLogger(className);
-        FileHandler fileHandler = null;
-        try {
-            fileHandler = new FileHandler("src//main//LogsForHttpServer//" + className + ".log");
-        } catch (IOException e) {
-            System.out.println("Unable to open/write Log File...");
-        }
-        logger.addHandler(fileHandler);
-        SimpleFormatter formatter = new SimpleFormatter();
-        fileHandler.setFormatter(formatter);
+//        logger = Logger.getLogger(className);
+//        FileHandler fileHandler = null;
+//        try {
+//            fileHandler = new FileHandler("src//main//LogsForHttpServer//" + className + ".log");
+//        } catch (IOException e) {
+//            System.out.println("Unable to open/write Log File...");
+//        }
+//        logger.addHandler(fileHandler);
+//        SimpleFormatter formatter = new SimpleFormatter();
+//        fileHandler.setFormatter(formatter);
     }
 
     public void writeLog(String message) {
-        logger.info(message);
+        System.out.println(message);
+//        logger.info(message);
     }
 
     public void writeLog(Level level, String message) {
-        logger.log(level, message);
+//        logger.log(level, message);
+        System.out.println(message);
     }
 
     public void writeLog(Level level, String message, Object object) {
-        logger.log(level, message, object);
+//        logger.log(level, message, object);
+        System.out.println(message);
     }
 }
